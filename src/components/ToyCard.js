74fb576afa555345f4a5c4a7f44035a4d928e4d7
menachem margolis <margolismendel5728@gmail.com>
2021-03-01 19:55:06 -0500

    Done.

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,19 +1,52 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({name, image, likes, id, toys, getNewLikes }) {
+
+ function donateToy(e){
+   fetch(`http://localhost:3001/toys/${id}`,{
+     method: "DELETE",
+     headers:{"content-type":"application-json"}
+   })
+   
+    e.target.closest('div').remove()
+
+ }
+
+ function increaseLikes(){
+    let like = {likes:likes += 1}
+    
+    fetch(`http://localhost:3001/toys/${id}`,{
+      method: "PATCH",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(like)
+    })
+    .then(res => res.json())
+    .then(data => {
+      let newLikes = data.likes
+      getNewLikes(id, newLikes)
+    })
+ }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button onClick={increaseLikes} className="like-btn">Like {"<3"}</button>
+      <button  onClick={donateToy}className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
 
 export default ToyCard;
+
+
+// - _When the like button is clicked_, make a PATCH request to `/toys/:id` with
+//   the id of the toy that was clicked, along with the new number of likes (this
+//   should be sent in the body of the PATCH request, as a object:
+//   `{ likes: 10 }`), to update the toy on the server. Clicking on the button
+//   should also increase the number of likes on the DOM.
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, getNewLikes}) {
+ 
+
+ const renderAllToys = toys.map((t) => {
+   return (<ToyCard getNewLikes={getNewLikes} toys = {toys} key ={t.id} name={t.name} image={t.image} likes={t.likes} id={t.id} />)
+ })
+ 
+ console.log(toys)
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{renderAllToys}</div>
   );
 }
 
 export default ToyContainer;
+
+
+// - _When the `ToyForm` is submitted_, make a POST request to `/toys` to save a
+//   new toy to the server. Using the ideas of controlled form and inverse data
+//   flow, think about how to render a new `ToyCard` for the toy that you created.
+
+// - _When the `Donate to Goodwill` button is clicked_, make a DELETE request to
+//   `/toys/:id` with the ID of the toy that was clicked to delete the toy from the
+//   server. The `ToyCard` that you clicked on should also be removed from the DOM.
+
+// - _When the like button is clicked_, make a PATCH request to `/toys/:id` with
+//   the id of the toy that was clicked, along with the new number of likes (this
+//   should be sent in the body of the PATCH request, as a object:
+//   `{ likes: 10 }`), to update the toy on the server. Clicking on the button
+//   should also increase the number of likes on the DOM.
