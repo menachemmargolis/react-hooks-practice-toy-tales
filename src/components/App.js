762e977acbb09872ed1c7de317044a456f92d9b5
menachem margolis <margolismendel5728@gmail.com>
2021-03-01 19:54:55 -0500

    commit tree 593019e9d3fe5227dbf6a657f2faf73a33135359

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,39 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  
+    const [toys, setToys] = useState([])
+  
+  
+   useEffect(() =>{
+     fetch(`http://localhost:3001/toys`)
+     .then(res => res.json())
+     .then(setToys)
+   },[])
+
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+  
+  function showUpdatedLikes(id, newLikes){
+   const filter =  toys.map((t) => {
+      return t.id === id ? {...t, likes: newLikes} : t
+    })
+
+    setToys(filter)
+  }
+
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm toyState={toys} setToyState={setToys}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer getNewLikes={showUpdatedLikes}  toys ={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({toyState, setToyState}) {
+
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+
+  
+
+  function addToyToGrid(e){
+    e.preventDefault()
+
+    const newToy ={
+     name,
+     image,
+     likes:0
+    }
+
+    fetch(`http://localhost:3001/toys`,{
+      method:"POST",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(newToy)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const add = [...toyState,data]
+      setToyState(add)
+    })
+
+  }
+    
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form onSubmit={addToyToGrid} className="add-toy-form">
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={(e) => setName(e.target.value)}
         />
         <br />
         <input
@@ -17,6 +47,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={(e) => setImage(e.target.value)}
         />
         <br />
         <input
@@ -31,3 +62,18 @@ function ToyForm() {
 }
 
 export default ToyForm;
+
+
+// - _When the `ToyForm` is submitted_, make a POST request to `/toys` to save a
+//   new toy to the server. Using the ideas of controlled form and inverse data
+//   flow, think about how to render a new `ToyCard` for the toy that you created.
+
+// - _When the `Donate to Goodwill` button is clicked_, make a DELETE request to
+//   `/toys/:id` with the ID of the toy that was clicked to delete the toy from the
+//   server. The `ToyCard` that you clicked on should also be removed from the DOM.
+
+// - _When the like button is clicked_, make a PATCH request to `/toys/:id` with
+//   the id of the toy that was clicked, along with the new number of likes (this
+//   should be sent in the body of the PATCH request, as a object:
+//   `{ likes: 10 }`), to update the toy on the server. Clicking on the button
+//   should also increase the number of likes on the DOM.
